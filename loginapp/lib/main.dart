fe5001fb66d2edbce9b22ef83b43fa45642c2704
Ba Thanh <thanhnb1101@gmail.com>
2023-09-18 17:55:02 +0700

    test commit gitdesktop

--- loginapp/lib/main.dart
+++ loginapp/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Login(title: 'Login App'),
+      home: const Login(title: 'Login App Demo'),
     );
   }
 }
